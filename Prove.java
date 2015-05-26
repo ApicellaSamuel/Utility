9f86ed6d13dc0c3874223f2e2fe2bfe81d21952c
ApicellaSamuel <samuelapicella@hotmail.com>
2015-05-26 17:01:12 +0200

    Update Prove.java

--- Prove.java
+++ Prove.java
@@ -2,10 +2,12 @@
 public class Prove {
 
 	public static void main(String[] args) {
-		String s[] = {"ok","no ","Si","ok!!","ok!!"};
-		Utils.DPair<String,Integer> r =  Utils.DPair.rep(s);
-		System.out.println(r);
-		
+		String strA[] = {"ok","no ","Si","ok!!","ok!!"};
+		Integer intA[] = {1,3,5,3,87,2,8,2};
+		Utils.DPair<String,Integer> pairA =  Utils.DPair.rep(strA);
+		Utils.DPair<Integer,Integer>  pairB=  Utils.DPair.rep(intA);
+		System.out.println(pairA.getFirst()+" "+pairA.getSecond());
+		System.out.println(pairB.getFirst()+" "+pairB.getSecond());
 	}
 
 }
