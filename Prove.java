b8c3061677054e8e44e58b3f0775314a2f6d5ab4
ApicellaSamuel <samuelapicella@hotmail.com>
2015-05-26 16:20:00 +0200

    Create Prove.java

---
+++ Prove.java
@@ -0,0 +1,11 @@
+
+public class Prove {
+
+	public static void main(String[] args) {
+		String s[] = {"ok","no ","Si","ok!!","ok!!"};
+		Utils.DPair<String,Integer> r =  Utils.DPair.rep(s);
+		System.out.println(r);
+		
+	}
+
+}
